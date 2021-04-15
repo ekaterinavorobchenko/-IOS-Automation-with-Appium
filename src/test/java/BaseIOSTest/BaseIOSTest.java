1bb4af01823ecbfc1bb998dd05bb33f07a64f7d0
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-15 09:52:53 +0300

    new URL

--- src/test/java/BaseIOSTest/BaseIOSTest.java
+++ src/test/java/BaseIOSTest/BaseIOSTest.java
@@ -1,12 +1,10 @@
 package BaseIOSTest;
 
-import io.appium.java_client.MobileElement;
+
 import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.IOSMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,14 +16,14 @@ public class BaseIOSTest {
 
       capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.4");
       capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"iPhone 12 Pro");
-      capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+      capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
       capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT,500000);
       capabilities.setCapability("commandTimeouts","12000");
       capabilities.setCapability(MobileCapabilityType.APP, "/Users/ekaterinavolobchenko/Library/Developer" +
               "/Xcode/DerivedData/UIKitCatalog-gfqsjfoddmuxlyaoxvhaitcyngeo/Build/Products/Debug-iphonesimulator" +
               "/UIKitCatalog.app");
 
-      URL url = new URL("http://localhost:4723/wd/hub");
+      URL url = new URL("http://0.0.0.0:4723/wd/hub");
       IOSDriver driver = new IOSDriver(url,capabilities);
       String sessionId = driver.getSessionId().toString();
    }
