ad02cb6f739785b9f99c35ee029eb82d7e0496b6
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-16 09:59:36 +0300

    Automate Sliders with IOS Element

--- src/test/java/SliderTest/SliderTest.java
+++ src/test/java/SliderTest/SliderTest.java
@@ -1,2 +1,19 @@
-package SliderTest;public class SliderTest {
+package SliderTest;
+
+import BaseIOSTest.BaseIOSTest;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+import org.testng.Assert;
+
+import java.net.MalformedURLException;
+
+public class SliderTest extends BaseIOSTest  {
+    public static void main(String[] args) throws MalformedURLException {
+        IOSDriver driver = DesiredCapabilities();
+        driver.findElementByAccessibilityId("Sliders").click();
+        IOSElement slider = (IOSElement) driver.findElementByXPath("//XCUIElementTypeSlider");
+        slider.setValue("0%");
+        slider.setValue("0.5%");
+        Assert.assertEquals("100%",slider.getAttribute("value"));
+    }
 }
