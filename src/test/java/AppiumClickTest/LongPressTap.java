4ef3da11496bb8d2b8bbc461f32c70490969ceb3
ekaterinavorobchenko <ekaterina.vorobchenko@letmecode.org>
2021-04-16 10:55:08 +0300

    Performing tap and longPress

---
+++ src/test/java/AppiumClickTest/LongPressTap.java
@@ -0,0 +1,22 @@
+package AppiumClickTest;
+
+import BaseIOSTest.BaseIOSTest;
+import io.appium.java_client.ios.IOSDriver;
+
+import java.net.MalformedURLException;
+
+public class AppiumClickTest extends BaseIOSTest
+{
+    public static void main(String[] args) throws MalformedURLException
+    {
+        IOSDriver driver = DesiredCapabilities();
+        driver.findElementByAccessibilityId("Alert Views").click();
+        //tagname[@attribute='value']
+        driver.findElementByXPath("//XCUIElementTypeStaticText[@name ='Text Entry']").click();
+        driver.findElementByXPath("//XCUIElementTypeCell").sendKeys("Hello");
+        driver.findElementByAccessibilityId("OK").click();
+        driver.findElementByAccessibilityId("Confirm / Cancel").click();
+        System.out.println(driver.findElementByXPath("//*[contains(@name,'message')]").getText());
+        driver.findElementByAccessibilityId("Confirm").click();
+    }
+}
--- src/test/java/BaseIOSTest/BaseIOSTest.java
+++ src/test/java/BaseIOSTest/BaseIOSTest.java
@@ -10,21 +10,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BaseIOSTest {
-   public static void main(String[] args) throws MalformedURLException {
+   public static IOSDriver DesiredCapabilities() throws MalformedURLException {
 
       DesiredCapabilities capabilities = new DesiredCapabilities();
 
       capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.4");
+      capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"IOS");
       capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"iPhone 12 Pro");
       capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
       capabilities.setCapability(IOSMobileCapabilityType.LAUNCH_TIMEOUT,500000);
       capabilities.setCapability("commandTimeouts","12000");
       capabilities.setCapability(MobileCapabilityType.APP, "/Users/ekaterinavolobchenko/Library/Developer" +
-              "/Xcode/DerivedData/UIKitCatalog-gfqsjfoddmuxlyaoxvhaitcyngeo/Build/Products/Debug-iphonesimulator" +
-              "/UIKitCatalog.app");
+              "/Xcode/DerivedData/longtap-cibktpwbaeyrfrgplccsbijsuqoa/Build/Products/Debug-iphonesimulator" +
+              "/longtap.app");
 
       URL url = new URL("http://0.0.0.0:4723/wd/hub");
       IOSDriver driver = new IOSDriver(url,capabilities);
-      String sessionId = driver.getSessionId().toString();
+      return driver;
    }
 }
